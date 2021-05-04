d6e3966a6c7835aae1da8f6762e55e2359e2be3f
lxvm <lorenzo@vanmunoz.com>
2021-05-04 16:00:42 -0700

    retroactively adding build script to hw1

--- hw1/README.md
+++ hw1/README.md
@@ -1,3 +1,12 @@
 # Transverse-Field Ising Model
 
 ``You can only be certain of the Heisenbug Uncertainty Principle''
+
+## Building this project
+
+You may run `$ ./make_this.sh` to build the project.
+It will probably take at least an hour to do the calculations.
+So read the script before executing it since there might be
+changes necessary before it works on another machine.
+More building details in the subdirectories, though the script
+shows how to do them all.
---
+++ hw1/make_this.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+# Run this script to build the whole project
+# You are warned that the calculations may take
+# in excess of 1 hour
+# This script contains the commands I executed
+# when building the project, though it may need
+# some adjustments depending on the operating
+# system or the location of certain directories
+# or the latex installation or gnuplot.
+# It should be executed from the current directory
+# The only output you should expect are the runtimes
+# in Fortran, which will also be saved to a file
+
+# load intel
+INTEL_START_SCRIPT=/opt/intel/oneapi/setvars.sh
+. $INTEL_START_SCRIPT intel64 mod
+
+# Move to project dir
+cd $(dirname `realpath $0`)
+
+# Fortran
+cd fortran
+# compile
+. bin/compile_lines.txt
+# run programs
+for i in `ls bin | grep -v compile_lines.txt`
+do
+  ./bin/$i | tee -a data/runtimes.txt
+done
+cd -
+
+cd gnuplot
+# Make plots
+. processing.sh
+
+# Make pdf
+cd -
+cd tex
+latexmk main.tex
