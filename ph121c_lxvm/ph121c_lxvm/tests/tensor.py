d644975e7a1f1b65162cb6a0e13b01c747b8d0ea
lxvm <lorenzo@vanmunoz.com>
2021-05-14 01:20:53 -0700

    split basis

--- ph121c_lxvm/ph121c_lxvm/tests/tensor.py
+++ ph121c_lxvm/ph121c_lxvm/tests/tensor.py
@@ -1,92 +1,30 @@
-"""Test procedures made available by ph121c_lxvm.basis subpackage.
+"""Test procedures made available by ph121c_lxvm.tensor subpackage.
 
-Modules and procedures/classes/methods tested:
-schmidt: permute, matricize, vectorize
-bits:    swap
+Classes and methods tested:
+mps: __init__, lower_rank, inner
 
 Untested (but otherwise verified) procedures:
-bits: poppar, vpoppar, Ising_parity_lookup, btest
-combinatorics: cycles
-unitary: Ising, Hadamard
+mps: oper
+mpo
 """
 
 import unittest
-from itertools import combinations
+from itertools import product, combinations
 
 import numpy as np
 
-from .. import basis
+from .. import tensor
 
-
-class bits_test_case (unittest.TestCase):
-    def test_bit_swap (self):
-        """Test bit swap."""
-        n = 10
-        indices = np.arange(2 ** n)
-        # swap test
-        for i, j in combinations(np.arange(n), r=2):
-            swapped = basis.bits.swap(indices, i, j)
-            self.assertTrue(np.all(
-                (swapped == indices)
-                ^ (((indices ^ swapped) - (2 ** i + 2 ** j)) == 0)
-            ))
-        # null test
-        for i in range(n):
-            self.assertTrue(
-                np.all(indices == basis.bits.swap(indices, i, i))
-            )
-            
-class schmidt_test_case (unittest.TestCase):
-    """Test the basis.schmidt module."""
-    
-    def test_permute (self):
-        """Check that the permutation and its inverse is the identity."""
-        for L in [3, 4, 5, 6, 7]:
-            v = np.random.random(2**L)
-            for r in range(1, L):
-                for c in combinations(range(L), r=r):
-                    with self.subTest(L=L, r=r, c=c):
-                        w = basis.schmidt.permute(v, c, L)
-                        w = basis.schmidt.permute(w, c, L, inverse=True)
-                        self.assertTrue(all(v == w))
-    
-    def test_matricization (self):
-        """Check that matricization and vectorization is the identity."""
-        for L in [3, 4, 5, 6, 7]:
-            v = np.random.random(2**L)
-            for r in range(1, L):
-                for c in combinations(range(L), r=r):
-                    with self.subTest(L=L, r=r, c=c):
-                        M = basis.schmidt.matricize(v, c, L)
-                        self.assertTrue(
-                            M.shape == (2 ** len(c), 2 ** (L - len(c)))
-                        )
-                        w = basis.schmidt.vectorize(M, c, L)
-                        self.assertTrue(
-                            w.shape in [(2**L, ), (2**L, 1)]
-                        )
-                        self.assertTrue(
-                            np.allclose(v, w)
-                        )
-    
-    def test_svd_reconstruction (self):
-        """Check that svd reconstruction is correct."""
-        return NotImplemented
-    
-    def test_schmidt_truncation (self):
-        """Test truncation of svd is correct"""
-        return NotImplemented
-
-class tensor_test_case (unittest.TestCase):
-    """Test the basis.mps module."""
-    def test_mps_accuracy (self):
+class mps_test_case (unittest.TestCase):
+    """Test the tensor.mps module."""
+    def test_accuracy (self):
         """Check that the compression works for several systems."""
         for d, L in product([2, 3], [5, 6, 7, 8]):
             v = np.random.random(d ** L)
             v = v / np.linalg.norm(v)
             def lossless (i):
-                return basis.tensor.dim_mps(i, L, d)
-            A = basis.tensor.mps(v, lossless, L, d)
+                return tensor.dim_mps(i, L, d)
+            A = tensor.mps(v, lossless, L, d)
             with self.subTest(name='Check lossless accuracy', d=d, L=L):
                 self.assertTrue(np.allclose(v, A.contract_bonds()))
             with self.subTest(name='Test monotonic quality of approximation', d=d, L=L):
@@ -101,7 +39,7 @@ class tensor_test_case (unittest.TestCase):
                     np.all(norms.argsort() == np.arange(norms.size)[::-1])
                 )
                 
-    def test_mps_inner_consistent (self):
+    def test_inner_consistent (self):
         """Compares the inner product calculated two ways.
         
         The first way is by contracting all of the bond indices.
@@ -111,8 +49,8 @@ class tensor_test_case (unittest.TestCase):
             v = np.random.random(d ** L)
             v = v / np.linalg.norm(v)
             def lossless (i):
-                return basis.tensor.dim_mps(i, L, d)
-            A = basis.tensor.mps(v, lossless, L, d)
+                return tensor.dim_mps(i, L, d)
+            A = tensor.mps(v, lossless, L, d)
             self.assertTrue(
                 np.allclose(np.inner(v, v), A.inner(A))
             )
@@ -125,5 +63,10 @@ class tensor_test_case (unittest.TestCase):
         """
         pass
     
+class mpo_test_case (unittest.TestCase):
+    """Test the tensor.mpo module."""
+    pass
+
+
 if __name__ == '__main__':
     unittest.main()
