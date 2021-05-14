5cb24c410283c16708261fac06dbadb4e9828014
lxvm <lorenzo@vanmunoz.com>
2021-05-14 01:24:11 -0700

    split basis

--- ph121c_lxvm/ph121c_lxvm/tests/basis.py
+++ ph121c_lxvm/ph121c_lxvm/tests/basis.py
@@ -68,62 +68,7 @@ class schmidt_test_case (unittest.TestCase):
                         self.assertTrue(
                             np.allclose(v, w)
                         )
-    
-    def test_svd_reconstruction (self):
-        """Check that svd reconstruction is correct."""
-        return NotImplemented
-    
-    def test_schmidt_truncation (self):
-        """Test truncation of svd is correct"""
-        return NotImplemented
-
-class tensor_test_case (unittest.TestCase):
-    """Test the basis.mps module."""
-    def test_mps_accuracy (self):
-        """Check that the compression works for several systems."""
-        for d, L in product([2, 3], [5, 6, 7, 8]):
-            v = np.random.random(d ** L)
-            v = v / np.linalg.norm(v)
-            def lossless (i):
-                return basis.tensor.dim_mps(i, L, d)
-            A = basis.tensor.mps(v, lossless, L, d)
-            with self.subTest(name='Check lossless accuracy', d=d, L=L):
-                self.assertTrue(np.allclose(v, A.contract_bonds()))
-            with self.subTest(name='Test monotonic quality of approximation', d=d, L=L):
-                ranks = np.arange(1 + d ** (L // 2), step=d)[::-1]
-                norms = np.zeros(ranks.size)
-                for i, chi in enumerate(ranks):
-                    def rank (i, x=chi):
-                        return max(1, min(x, lossless(i)))
-                    A.lower_rank(rank)
-                    norms[i] = np.inner(v, A.contract_bonds())
-                self.assertTrue(
-                    np.all(norms.argsort() == np.arange(norms.size)[::-1])
-                )
-                
-    def test_mps_inner_consistent (self):
-        """Compares the inner product calculated two ways.
-        
-        The first way is by contracting all of the bond indices.
-        The second way is by performing an mps inner product.
-        """
-        for d, L in product([2, 3], [5, 6, 7, 8]):
-            v = np.random.random(d ** L)
-            v = v / np.linalg.norm(v)
-            def lossless (i):
-                return basis.tensor.dim_mps(i, L, d)
-            A = basis.tensor.mps(v, lossless, L, d)
-            self.assertTrue(
-                np.allclose(np.inner(v, v), A.inner(A))
-            )
-    
-    def test_local_oper_consistent (self):
-        """Test expectation value of a local operator two ways.
-        
-        The first way is by calculating the operator as a matrix-vector product.
-        The second way is by performing an mps inner product.
-        """
-        pass
+                        
     
 if __name__ == '__main__':
     unittest.main()
