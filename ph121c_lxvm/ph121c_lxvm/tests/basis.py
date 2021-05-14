d6df858f846ca0b589144c11d6bbd0444b70c99c
lxvm <lorenzo@vanmunoz.com>
2021-05-14 01:09:01 -0700

    combine bits and basis

--- ph121c_lxvm/ph121c_lxvm/tests/basis.py
+++ ph121c_lxvm/ph121c_lxvm/tests/basis.py
@@ -18,7 +18,24 @@ import numpy as np
 from .. import basis
 
 
-
+class bits_test_case (unittest.TestCase):
+    def test_bit_swap (self):
+        """Test bit swap."""
+        n = 10
+        indices = np.arange(2 ** n)
+        # swap test
+        for i, j in combinations(np.arange(n), r=2):
+            swapped = basis.bits.swap(indices, i, j)
+            self.assertTrue(np.all(
+                (swapped == indices)
+                ^ (((indices ^ swapped) - (2 ** i + 2 ** j)) == 0)
+            ))
+        # null test
+        for i in range(n):
+            self.assertTrue(
+                np.all(indices == basis.bits.swap(indices, i, i))
+            )
+            
 class schmidt_test_case (unittest.TestCase):
     """Test the basis.schmidt module."""
     
