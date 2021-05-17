65749b25b299e29c1cf537e61e6f2b852e92e868
lxvm <lorenzo@vanmunoz.com>
2021-05-17 13:34:36 -0700

    Thank k you Shu Fay! Problem with Rydberg Hamiltonian is solved

--- ph121c_lxvm/ph121c_lxvm/fortran/scars.f90
+++ ph121c_lxvm/ph121c_lxvm/fortran/scars.f90
@@ -68,89 +68,28 @@ contains
         ! Note: N = 2 ** L
         integer,    intent (in   ) :: L, N
         real(dp),   intent (in   ) :: O
-        complex(dp),intent (in   ) :: v(0:(N - 1))
-        complex(dp),intent (  out) :: w(0:(N - 1))
-        complex(dp) cj0, cj1, cjj
-        integer i, j, m, zj0, zj1, zj2
-        logical bj0, bj1
+        real(dp),intent (in   ) :: v(0:(N - 1))
+        real(dp),intent (  out) :: w(0:(N - 1))
+        integer i, j, m
         w = 0
         
         do i = 0, ((2 ** L) - 1)
             if (v(i) /= 0) then
-                ! Diagonal (0 Hamming distance) 1-site and 3-site sigma z terms
+                ! Diagonal (0 Hamming distance) 1-site - 3-site sigma z terms
                 w(i) = w(i) + v(i) * (sz_k_diag(i, L, 1) - sz_k_diag(i, L, 3)) / 4
                 ! Off-diagonal terms (3-site and transverse 1-site sigma x)
-                do j = 0, (L - 1)
-                    ! Find states of sites j and j + 1
-                    bj0 = btest(i, j)
-                    bj1 = btest(i, mod(j + 1, L))
-                    ! Find signs of sz terms at the 3 sites
-                    if (bj0) then
-                        zj0 = -1
-                    else
-                        zj0 = 1
-                    end if
-                    if (bj1) then
-                        zj1 = -1
-                    else
-                        zj1 = 1
-                    end if
-                    if (btest(i, mod(j + 2, L))) then
-                        zj2 = -1
-                    else
-                        zj2 = 1
-                    end if
-                    
-                    ! Terms of Hamming-distance 1 at position j
+                do j = 0, (L - 1)                  
+                    ! Terms of Hamming-distance 1 at position j (transverse sx)
                     m = ieor(i, 2 ** j)
-                    ! sx sz sz term
-                    cj0 = 1
-                    ! sy sz sz term
-                    if (bj0) then
-                        cj0 = cj0 - Im
-                    else
-                        cj0 = cj0 + Im
-                    end if
-                    ! Set value (including the transverse 1-site sigma x term)
-                    w(m) = w(m) + v(i) * ((O / 2) - ((cj0 * zj1 * zj2) / 4))
-                    
-                    ! Terms of Hamming-distance 1 at position j + 1
-                    m = ieor(i, 2 ** mod(j + 1, L))
-                    ! sz sx sz term
-                    cj1 = 1
-                    ! sz sy sz term
-                    if (bj1) then
-                        cj1 = cj1 - Im
-                    else
-                        cj1 = cj1 + Im
-                    end if
-                    ! Set value
-                    w(m) = w(m) + v(i) * (-cj1) * zj0 * zj2 / 4
-
+                    w(m) = w(m) + v(i) * (O / 2)
                     ! Terms of Hamming distance 2 at positions j, j + 1
-                    m = ieor(ieor(i, 2 ** j), 2 ** mod(j + 1, L))
-                    ! sx sx sz term
-                    cjj = 1
-                    ! sx sy sz term
-                    if (bj1) then
-                        cjj = cjj - Im
-                    else
-                        cjj = cjj + Im
-                    end if
-                    ! sy sx sz term
-                    if (bj0) then
-                        cjj = cjj - Im
-                    else
-                        cjj = cjj + Im
-                    end if
-                    ! sy sy sz term
-                    if (bj0 .eqv. bj1) then
-                        cjj = cjj - 1
-                    else
-                        cjj = cjj + 1
-                    end if
+                    m = ieor(m, 2 ** mod(j + 1, L))
                     ! Set value
-                    w(m) = w(m) + v(i) * (-cjj) * zj2 / 4
+                    w(m) = w(m) + v(i) * (-0.25) &
+                        ! sx sx sz + sy sy sz terms (1 - 1 = 0)
+                        * (2 * poppar(iand(i, (2 ** j) + (2 ** mod(j + 1, L))))) &
+                        ! sign of sz
+                        * (1 - 2 * poppar(iand(i, 2 ** mod(j + 2, L))))
                 end do
             end if
         end do
--- ph121c_lxvm/ph121c_lxvm/models/scars.py
+++ ph121c_lxvm/ph121c_lxvm/models/scars.py
@@ -23,8 +23,8 @@ def H_oper (L, O):
 
 def H_dense (L, O):
     """Return the dense Hamiltonian"""
-    H = np.zeros((2 ** L, 2 ** L), dtype='complex')
-    v = np.zeros(2 ** L, dtype='complex')
+    H = np.zeros((2 ** L, 2 ** L))
+    v = np.zeros(2 ** L)
     for i in range(2 ** L):
         v[i]    = 1
         H[:, i] = H_vec(v, L, O)
@@ -34,9 +34,9 @@ def H_dense (L, O):
 def H_kron (L, O):
     """Build the dense Hamiltonian as a Kronecker product (mostly for testing)."""
     terms = []
-    sx = np.array([[0, 1],  [1,  0]], dtype='complex')
-    sy = np.array([[0,-1j], [1j, 0]], dtype='complex')
-    sz = np.array([[1, 0],  [0, -1]], dtype='complex')
+    sx = np.array([[0, 1],  [1,  0]])
+    sy = np.array([[0,-1j], [1j, 0]])
+    sz = np.array([[1, 0],  [0, -1]])
     for i in range(L):
         # Transverse sx
         terms.append(mpo(L, d=2))
@@ -44,11 +44,11 @@ def H_kron (L, O):
         # 1-local sz term
         terms.append(mpo(L, d=2))
         terms[-1][(i + 2) % L] = sz / 4
-        # 3-site befuddler
-        for a, b in product([sx, sy, sz], repeat=2):
-            # a b sz term
+        # 3-site befuddler: a dot product of vector operators
+        for e in [sx, sy, sz]:
             terms.append(mpo(L, d=2))
-            terms[-1][i] = - a / 4
-            terms[-1][(i + 1) % L] = b
+            terms[-1][i] = - e / 4
+            terms[-1][(i + 1) % L] = e
             terms[-1][(i + 2) % L] = sz
-    return sum(e.toarray() for e in terms)
+        # Use the fact kron(sy, sy) is real to simplify
+    return sum(np.real(e.toarray()) for e in terms)
